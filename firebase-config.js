e18f72362c1d3c3cfb724b45e6c1f8b5c3715f7a
HR-Suchock <suchock849@gmail.com>
2025-07-22 23:30:46 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,1 @@
-// Firebase v8 Config
-const firebaseConfig = {
-  apiKey: "AIzaSyA1Lx114VTqRNU2sM0rfEqwLSuZyoSefqs",
-  authDomain: "friends-zone-c2fe8.firebaseapp.com",
-  projectId: "friends-zone-c2fe8",
-  storageBucket: "friends-zone-c2fe8.firebasestorage.app",
-  messagingSenderId: "962940178101",
-  appId: "1:962940178101:web:330aa526d4e7b012dff8fc",
-  measurementId: "G-VPGKVYTKD1"
-};
 
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
